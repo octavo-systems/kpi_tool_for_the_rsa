64bbf301a29e11d4896e77d2f93b91d06091d39c
aw@rationalcommerce.com <aw@rationalcommerce.com>
2023-07-27 07:53:13 +0000

    Created SIFTER_INTERFACE; Edited Form1.RowTemplate1 and Form1

---
+++ server_code/SIFTER_INTERFACE.py
@@ -0,0 +1,189 @@
+import anvil.server
+import requests
+import json
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+class Category(object):
+    """Representation of a Category in Sifter"""
+    def __init__(self, category):
+        self.name = category['name']
+        self.issues_url = category['issues_url']
+        self.api_issues_url = category['api_issues_url']
+
+class Comment(object):
+    """Representation of an Issue in Sifter"""
+    def __init__(self, issue):
+        self.category = issue['category']
+        self.body = issue['body']
+        self.priority = issue['priority']
+        self.commenter = issue['commenter']
+        self.milestone_name = issue['milestone_name']
+        self.assignee_name = issue['assignee_name']
+        self.status = issue['status']
+        self.created_at = issue['created_at']
+        self.updated_at = issue['updated_at']
+
+class Comment(object):
+    """Representation of an Issue in Sifter"""
+    def __init__(self, issue):
+        self.category = issue['category']
+        self.body = issue['body']
+        self.priority = issue['priority']
+        self.commenter = issue['commenter']
+        self.milestone_name = issue['milestone_name']
+        self.assignee_name = issue['assignee_name']
+        self.status = issue['status']
+        self.created_at = issue['created_at']
+        self.updated_at = issue['updated_at']
+
+class User(object):
+    """Representation of a User in Sifter"""
+    def __init__(self, user):
+        self.username = user['username']
+        self.first_name = user['first_name']
+        self.last_name = user['last_name']
+        self.email = user['email']
+        self.issues_url = user['issues_url']
+        self.api_issues_url = user['api_issues_url']
+
+class Milestone(object):
+    """Representation of a Milestone in Sifter"""
+    def __init__(self, milestone):
+        self.name = milestone['name']
+        self.due_date = milestone['due_date']
+        self.issues_url = milestone['issues_url']
+        self.api_issues_url = milestone['api_issues_url']
+
+
+class Issue(object):
+    """Representation of an Issue in Sifter"""
+    def __init__(self, issue, account):
+        self._account = account
+        self.number = issue['number']
+        self.subject = issue['subject']
+        self.description = issue['description']
+        self.priority = issue['priority']
+        self.status = issue['status']
+        self.assignee_name = issue['assignee_name']
+        self.category_name = issue['category_name']
+        self.milestone_name = issue['milestone_name']
+        self.opener_name = issue['opener_name']
+        self.url = issue['url']
+        self.api_url = issue['api_url']
+        self.comment_count = issue['comment_count']
+        self.created_at = issue['created_at']
+        self.updated_at = issue['updated_at']
+
+    def comments(self):
+        """Gets Comments for a given Issue"""
+        comments = []
+        json_raw = self._account.request(self.api_url)
+        raw_issue = json_raw['issue']
+        raw_comments = raw_issue['comments']
+        for raw_comment in raw_comments:
+            c = comment.Comment(raw_comment)
+            comments.append(c)
+
+        return comments
+
+class Project(object):
+    """Representation of a project in Sifter"""
+    def __init__(self, project, account):
+        self._account = account
+        self.issues_url = project['issues_url']
+        self.archived = project['archived']
+        self.url = project['url']
+        self.api_url = project['api_url']
+        self.milestones_url = project['milestones_url']
+        self.api_people_url = project['api_people_url']
+        self.api_issues_url = project['api_issues_url']
+        self.api_milestones_url = project['api_milestones_url']
+        self.api_categories_url = project['api_categories_url']
+        self.name = project['name']
+        self.primary_company_name = project['primary_company_name']
+
+    def issues(self):
+        """Gets all the issues for a given project"""
+        issues = []
+
+        # Set per_page to 25 issues per page
+        first_page = self.api_issues_url + '?per_page=25&page=1'
+
+        # Get page one
+        json_raw = self._account.request(first_page)
+
+        # Set the next page
+        next_page = json_raw['next_page_url']
+
+        # Set the number of pages
+        number_of_pages = json_raw['total_pages']
+
+        for current_page in range(number_of_pages):
+            # Create a wrapper for each issue, add it to the list
+            raw_issues = json_raw['issues']
+            for raw_issue in raw_issues:
+                i = issue.Issue(raw_issue, self._account)
+                issues.append(i)
+
+            # Make a request for the next page
+            if current_page < number_of_pages - 1:
+                # store the results
+                json_raw = self._account.request(next_page)
+
+            # set the next page
+            next_page = json_raw['next_page_url']
+
+        return issues
+
+    def milestones(self):
+        """Gets all the milestones for a given project"""
+        milestones = []
+        json_raw = self._account.request(self.api_milestones_url)
+
+        raw_milestones = json_raw['milestones']
+        for raw_milestone in raw_milestones:
+            m = milestone.Milestone(raw_milestone)
+            milestones.append(m)
+
+        return milestones
+
+    def categories(self):
+        """Gets all the categories for a given project"""
+        categories = []
+        json_raw = self._account.request(self.api_categories_url)
+
+        raw_categories = json_raw['categories']
+        for raw_category in raw_categories:
+            c = category.Category(raw_category)
+            categories.append(c)
+
+        return categories
+
+    def people(self):
+        """Gets all the people for a given project"""
+        people = []
+        json_raw = self._account.request(self.api_people_url)
+
+        raw_people = json_raw['people']
+        for raw_user in raw_people:
+            u = user.User(raw_user)
+            people.append(u)
+
+        return people
+
+
+
+
+
+
