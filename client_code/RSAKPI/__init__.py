f3b749501aa7b529863b06ded56c887b4c5a2a23
aw@rationalcommerce.com <aw@rationalcommerce.com>
2023-10-11 11:57:39 +0000

    Edited RSAKPI

--- client_code/RSAKPI/__init__.py
+++ client_code/RSAKPI/__init__.py
@@ -5,24 +5,6 @@ import json
 import datetime
 import dateutil.parser 
 
-critical = 0
-high = 0
-normal = 0
-low = 0
-trivial = 0
-service_requests = 0
-system_logs = 0
-dvcsd_contacts = 0
-tickets_less_than_ten_days = 0
-tickets_more_than_sixty = 0
-
-openSifters = 0
-reopenedSifters = 0
-followupSifters = 0
-resolvedSifters = 0
-closedSifters = 0
-totalSifters = 0
-percentage_incidents_in_sla = 100
 
 class Category(object):
     """Representation of a Category in Sifter"""
@@ -272,162 +254,162 @@ class RSAKPI(RSAKPITemplate):
             failedresponse = []
 
             if kpi.priority == "Critical": 
-                critical = critical + 1
+                self.critical.text = self.critical.text + 1
                 if exceedsResponse(kpi,P1):
                     print(str(kpi.number)+' Exceeds response time')
                     failedresponse.append('P1 : '+str(kpi.number))
             elif kpi.priority == "High":
-                high = high + 1
+                self.high.text = self.high.text + 1
                 if exceedsResponse(kpi,P2):
                     print(str(kpi.number)+' Exceeds response time')
                     failedresponse.append('P2 : '+str(kpi.number))                
             elif kpi.priority == "Normal": 
-                normal = normal + 1                
+                self.normal.text = self.normal.text + 1                
                 if exceedsResponse(kpi,P3):
                     print(str(kpi.number)+' Exceeds response time')  
                     failedresponse.append('P3 : '+str(kpi.number))                                  
 
             elif kpi.priority == "Low":
-                low = low + 1                
+                self.low.text = self.low.text + 1                
                 if exceedsResponse(kpi,P4):
                     print(str(kpi.number)+' Exceeds response time') 
                     failedresponse.append('P4 : '+str(kpi.number))                                    
             elif kpi.priority == "Trivial":                
-                trivial = trivial + 1
+                self.trivial.text = self.trivial.text + 1
             else: 
                 print('Unexpected Priority found : '+ kpi.priority)
 
             if kpi.category_name == "Service Request":
-                service_requests = service_requests + 1
+                self.service_requests.text = self.service_requests.text + 1
 
             if kpi.subject[1:17].lower == "application error":
-                system_logs = system_logs + 1
+                self.system_logs.text = self.system_logs.text + 1
 
             if kpi.subject[1:5].lower == "dvcsd":
-                dvcsd_contacts = dvcsd_contacts + 1
+                self.dvcsd_contacts.text = self.dvcsd_contacts.text + 1
 
             if dateutil.parser.parse(kpi.created_at) + datetime.timedelta(days=10) >= datetime.datetime.now(tz):
-                tickets_less_than_ten_days = tickets_less_than_ten_days + 1
+                self.tickets_less_than_ten_days.text = self.tickets_less_than_ten_days.text + 1
 
             if dateutil.parser.parse(kpi.created_at) + datetime.timedelta(days=60) <= datetime.datetime.now(tz):
-                tickets_more_than_sixty = tickets_more_than_sixty + 1 
+                self.tickets_more_than_sixty.text = self.tickets_more_than_sixty.text + 1 
 
             if kpi.status == "Open":
-                openSifters = openSifters +1
+                self.open.text = self.open.text +1
             elif kpi.status == "Reopened":
-                reopenedSifters = reopenedSifters + 1
+                self.reopened.text = self.reopened.text + 1
             elif kpi.status == "Follow Up":
-                followupSifters = followupSifters + 1
+                self.followup.text = self.followup.text + 1
             elif kpi.status == "Resolved":
-                resolvedSifters = resolvedSifters + 1
+                self.resolved.text = self.resolved.text + 1
             elif kpi.status == "Closed":
-                closedSifters = closedSifters + 1
+                self.closed.text = self.closed.text + 1
             else: 
                 print('Unexpected status found : '+ kpi.status)  
 
-            totalSifters = openSifters + reopenedSifters + followupSifters + resolvedSifters + closedSifters
+            self.total.text = self.open.text + self.reopened.text + self.followup.text + self.resolved.text + self.closed.text
 
 
 
     print("New this month:")
-    print("Critical : " + str(critical))
-    print("High : " + str(high))
-    print("Normal : " + str(normal))
-    print("Low : " + str(low))
-    print("Trivial : " + str(trivial))
+    print("Critical : " + str(self.critical.text))
+    print("High : " + str(self.high.text))
+    print("Normal : " + str(self.normal.text))
+    print("Low : " + str(self.low.text))
+    print("Trivial : " + str(self.trivial.text))
 
     return
 
   def save_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-  with open('c:\\temp\\python_kpi_report.txt', 'a') as report:
-      HC_NEWLINE = '\n'
-      HC_TAB = '\t'
-      report.write( 'RSA KPI Report for '+'Test Month' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'Incident Management'+HC_NEWLINE )
-      report.write( HC_NEWLINE )
-      report.write( '1. Number of Incidents in month'+HC_NEWLINE )
-
-      report.write( HC_NEWLINE )
-      report.write( 'Critical  ' + HC_TAB + str(critical) + HC_NEWLINE )
-      report.write( 'High  '  + HC_TAB+HC_TAB + str(high) + HC_NEWLINE)	
-      report.write( 'Normal  ' + HC_TAB + str(normal)+ HC_NEWLINE)
-      report.write( 'Low  '  + HC_TAB+HC_TAB + str(low)+ HC_NEWLINE)
-      report.write( 'Trivial  ' + HC_TAB + str(trivial)+ HC_NEWLINE)
-
-      report.write( HC_NEWLINE +'Failed SLAs on SIFTER response time for: ' + HC_NEWLINE)
-
-      if len(failedresponse) == 0:
-          report.write( HC_NEWLINE +'None.' + HC_NEWLINE)
-      else:
-          report.write( HC_NEWLINE )
-          for kpifailure in failedresponse:
-              report.write( 'SIFTER  ' + HC_TAB + kpifailure + HC_NEWLINE )
-
-      report.write( HC_NEWLINE )
-      report.write( '1.1 SIFTER Volumes'+HC_NEWLINE )
-
-      report.write( HC_NEWLINE )
-      report.write( 'Open' + HC_TAB + HC_TAB+ str(openSifters) + HC_NEWLINE )
-      report.write( 'Reopened'  + HC_TAB + str(reopenedSifters) + HC_NEWLINE)    
-      report.write( 'Followup' + HC_TAB + str(followupSifters)+ HC_NEWLINE)
-      report.write( 'Resolved'  + HC_TAB + str(resolvedSifters)+ HC_NEWLINE)
-      report.write( 'Closed' + HC_TAB +HC_TAB+ str(closedSifters)+ HC_NEWLINE)    
-      report.write( 'Total' + HC_TAB +HC_TAB+ str(totalSifters)+ HC_NEWLINE)    
-
-      report.write( HC_NEWLINE )
-      report.write( '2. Percentage of incidents that met service levels (where no dependance on DVCSD)' +HC_NEWLINE)
-      report.write( HC_NEWLINE )    
-      report.write( 'Percentage  ' + HC_TAB +  str(percentage_incidents_in_sla) + '%' +HC_NEWLINE )
-      report.write( HC_NEWLINE )
-      report.write( '3. First contact resolution rate' +HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( 'Not Applicable  All incidents raised through SIFTER'+HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( '4. Number of incident backlog/aged tickets less than 10 days old (where no dependency on DVCSD)' +HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( 'Tickets  ' + str(tickets_less_than_ten_days)+HC_NEWLINE)        
-      report.write( HC_NEWLINE )
-      report.write( '5. Unplanned Outages split into DVCSD and Vendor issues' +HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( 'Not Applicable, Production is managed by DVCSD'+HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( '6. Number of Service requests' +HC_NEWLINE)
-      report.write( HC_NEWLINE )    
-      report.write( 'Tickets  ' + str(service_requests)+HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( '7. Percentage of service requests that met service levels.' +HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( 'Not Applicable, There is no SLA for service requests'+HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( '8. Number of SR aged tickets 60 days (where no dependency on DVCSD)' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'Tickets  ' + str(tickets_more_than_sixty)+HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( '9. Number of issues identified from system logs' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'Tickets  ' + str(system_logs)+HC_NEWLINE)    
-      report.write( HC_NEWLINE )        
-      report.write( '10. Number of alerts/communications from DVCSD actioned' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'Tickets  ' + str(dvcsd_contacts)+HC_NEWLINE)        
-      report.write( HC_NEWLINE )    
-      report.write( '11. System up time clarify what is DVCSD and Vendor related on downtime.' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'Not Applicable' +HC_NEWLINE)                
-
-      report.write( HC_NEWLINE )
-      report.write( 'Release Management'+HC_NEWLINE )
-      report.write( HC_NEWLINE )            
-
-
-      report.write( '1. Hypercare Exit on deployments major releases - Exit of hypercare after 1 month dependent no P1 or P2 raised by customer/ business in the last 7 days.' +HC_NEWLINE)
-      report.write( HC_NEWLINE )
-      report.write( 'N/A' +     HC_NEWLINE )
-      report.write( '2. Minor releases will have related hypercare of 2 weeks - no P1 or P2 raised by customer/business in the last 4 days to close that hypercare period.' +HC_NEWLINE)    
-      report.write( HC_NEWLINE )
-      report.write( 'N/A' + HC_NEWLINE )  
+    with open('c:\\temp\\python_kpi_report.txt', 'a') as report:
+        HC_NEWLINE = '\n'
+        HC_TAB = '\t'
+        report.write( 'RSA KPI Report for '+'Test Month' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'Incident Management'+HC_NEWLINE )
+        report.write( HC_NEWLINE )
+        report.write( '1. Number of Incidents in month'+HC_NEWLINE )
+  
+        report.write( HC_NEWLINE )
+        report.write( 'Critical  ' + HC_TAB + str(self.critical.text) + HC_NEWLINE )
+        report.write( 'High  '  + HC_TAB+HC_TAB + str(self.high.text) + HC_NEWLINE)	
+        report.write( 'Normal  ' + HC_TAB + str(self.normal.text)+ HC_NEWLINE)
+        report.write( 'Low  '  + HC_TAB+HC_TAB + str(self.low.text)+ HC_NEWLINE)
+        report.write( 'Trivial  ' + HC_TAB + str(self.trivial.text)+ HC_NEWLINE)
+  
+        report.write( HC_NEWLINE +'Failed SLAs on SIFTER response time for: ' + HC_NEWLINE)
+  
+        if len(failedresponse) == 0:
+            report.write( HC_NEWLINE +'None.' + HC_NEWLINE)
+        else:
+            report.write( HC_NEWLINE )
+            for kpifailure in failedresponse:
+                report.write( 'SIFTER  ' + HC_TAB + kpifailure + HC_NEWLINE )
+  
+        report.write( HC_NEWLINE )
+        report.write( '1.1 SIFTER Volumes'+HC_NEWLINE )
+  
+        report.write( HC_NEWLINE )
+        report.write( 'Open' + HC_TAB + HC_TAB+ str(self.open.text) + HC_NEWLINE )
+        report.write( 'Reopened'  + HC_TAB + str(self.reopened.text) + HC_NEWLINE)    
+        report.write( 'Followup' + HC_TAB + str(self.followup.text)+ HC_NEWLINE)
+        report.write( 'Resolved'  + HC_TAB + str(self.resolved.text)+ HC_NEWLINE)
+        report.write( 'Closed' + HC_TAB +HC_TAB+ str(self.closed.text)+ HC_NEWLINE)    
+        report.write( 'Total' + HC_TAB +HC_TAB+ str(self.total.text)+ HC_NEWLINE)    
+  
+        report.write( HC_NEWLINE )
+        report.write( '2. Percentage of incidents that met service levels (where no dependance on DVCSD)' +HC_NEWLINE)
+        report.write( HC_NEWLINE )    
+        report.write( 'Percentage  ' + HC_TAB +  str(self.percentage_incidents_in_sla.text) + '%' +HC_NEWLINE )
+        report.write( HC_NEWLINE )
+        report.write( '3. First contact resolution rate' +HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( 'Not Applicable  All incidents raised through SIFTER'+HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( '4. Number of incident backlog/aged tickets less than 10 days old (where no dependency on DVCSD)' +HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( 'Tickets  ' + str(self.tickets_less_than_ten_days.text)+HC_NEWLINE)        
+        report.write( HC_NEWLINE )
+        report.write( '5. Unplanned Outages split into DVCSD and Vendor issues' +HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( 'Not Applicable, Production is managed by DVCSD'+HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( '6. Number of Service requests' +HC_NEWLINE)
+        report.write( HC_NEWLINE )    
+        report.write( 'Tickets  ' + str(self.service_requests.text)+HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( '7. Percentage of service requests that met service levels.' +HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( 'Not Applicable, There is no SLA for service requests'+HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( '8. Number of SR aged tickets 60 days (where no dependency on DVCSD)' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'Tickets  ' + str(self.tickets_more_than_sixty.text)+HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( '9. Number of issues identified from system logs' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'Tickets  ' + str(self.system_logs.text)+HC_NEWLINE)    
+        report.write( HC_NEWLINE )        
+        report.write( '10. Number of alerts/communications from DVCSD actioned' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'Tickets  ' + str(self.dvcsd_contacts.text)+HC_NEWLINE)        
+        report.write( HC_NEWLINE )    
+        report.write( '11. System up time clarify what is DVCSD and Vendor related on downtime.' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'Not Applicable' +HC_NEWLINE)                
+  
+        report.write( HC_NEWLINE )
+        report.write( 'Release Management'+HC_NEWLINE )
+        report.write( HC_NEWLINE )            
+  
+  
+        report.write( '1. Hypercare Exit on deployments major releases - Exit of hypercare after 1 month dependent no P1 or P2 raised by customer/ business in the last 7 days.' +HC_NEWLINE)
+        report.write( HC_NEWLINE )
+        report.write( 'N/A' +     HC_NEWLINE )
+        report.write( '2. Minor releases will have related hypercare of 2 weeks - no P1 or P2 raised by customer/business in the last 4 days to close that hypercare period.' +HC_NEWLINE)    
+        report.write( HC_NEWLINE )
+        report.write( 'N/A' + HC_NEWLINE )  
 
 
 
