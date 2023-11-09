351450617f2e4c6952a3087235a656d702569e60
octavo-systems <146358696+octavo-systems@users.noreply.github.com>
2023-11-09 09:25:31 +0000

    division by zero protection

--- client_code/RSAKPI/__init__.py
+++ client_code/RSAKPI/__init__.py
@@ -44,11 +44,11 @@ class RSAKPI(RSAKPITemplate):
 
     self.sifterrows.items = critical.failedresponse+high.failedresponse+normal.failedresponse+low.failedresponse+trivial.failedresponse
 
-    if len(self.failedresponse.items) == 0:
+    if len(self.sifterrows.items) == 0:
       self.failed_response_list.text =self.failed_response_list.text + " (None this month)"
 
     if self.total_incidents.text != 0: 
-      self.percentage_incidents_in_sla.text = (1 - (len(self.failedresponse.items) / self.total_incidents.text)) * 100
+      self.percentage_incidents_in_sla.text = (1 - (len(self.sifterrows.items) / self.total_incidents.text)) * 100
     else:
       self.percentage_incidents_in_sla.text = 100      
     
@@ -62,94 +62,98 @@ class RSAKPI(RSAKPITemplate):
   
   def save_btn_click(self, **event_args):
     """This method is called when the save button is clicked"""
-    with open('c:\\temp\\kpi_report_'+self.month.selected_value+'_'+self.year.selected_value+'.txt', 'a') as report:
-        HC_NEWLINE = '\n'
-        HC_TAB = '\t'
-        report.write( 'RSA KPI Report for '+'Test Month' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'Incident Management'+HC_NEWLINE )
-        report.write( HC_NEWLINE )
-        report.write( '1. Number of Incidents in month'+HC_NEWLINE )
-  
-        report.write( HC_NEWLINE )
-        report.write( 'Critical  ' + HC_TAB + str(self.critical.text) + HC_NEWLINE )
-        report.write( 'High  '  + HC_TAB+HC_TAB + str(self.high.text) + HC_NEWLINE)	
-        report.write( 'Normal  ' + HC_TAB + str(self.normal.text)+ HC_NEWLINE)
-        report.write( 'Low  '  + HC_TAB+HC_TAB + str(self.low.text)+ HC_NEWLINE)
-        report.write( 'Trivial  ' + HC_TAB + str(self.trivial.text)+ HC_NEWLINE)
-  
-        report.write( HC_NEWLINE +'Failed SLAs on SIFTER response time for: ' + HC_NEWLINE)
-  
-        if len(self.failedresponse) == 0:
-            report.write( HC_NEWLINE +'None.' + HC_NEWLINE)
-        else:
-            report.write( HC_NEWLINE )
-            for kpifailure in self.failedresponse:
-                report.write( 'SIFTER  ' + HC_TAB + kpifailure + HC_NEWLINE )
-  
-        report.write( HC_NEWLINE )
-        report.write( '1.1 SIFTER Volumes'+HC_NEWLINE )
-  
-        report.write( HC_NEWLINE )
-        report.write( 'Open' + HC_TAB + HC_TAB+ str(self.open.text) + HC_NEWLINE )
-        report.write( 'Reopened'  + HC_TAB + str(self.reopened.text) + HC_NEWLINE)    
-        report.write( 'Followup' + HC_TAB + str(self.followup.text)+ HC_NEWLINE)
-        report.write( 'Resolved'  + HC_TAB + str(self.resolved.text)+ HC_NEWLINE)
-        report.write( 'Closed' + HC_TAB +HC_TAB+ str(self.closed.text)+ HC_NEWLINE)    
-        report.write( 'Total' + HC_TAB +HC_TAB+ str(self.total.text)+ HC_NEWLINE)    
-  
-        report.write( HC_NEWLINE )
-        report.write( '2. Percentage of incidents that met service levels (where no dependance on DVCSD)' +HC_NEWLINE)
-        report.write( HC_NEWLINE )    
-        report.write( 'Percentage  ' + HC_TAB +  str(self.percentage_incidents_in_sla.text) + '%' +HC_NEWLINE )
-        report.write( HC_NEWLINE )
-        report.write( '3. First contact resolution rate' +HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( 'Not Applicable  All incidents raised through SIFTER'+HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( '4. Number of incident backlog/aged tickets less than 10 days old (where no dependency on DVCSD)' +HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( 'Tickets  ' + str(self.tickets_less_than_ten_days.text)+HC_NEWLINE)        
-        report.write( HC_NEWLINE )
-        report.write( '5. Unplanned Outages split into DVCSD and Vendor issues' +HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( 'Not Applicable, Production is managed by DVCSD'+HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( '6. Number of Service requests' +HC_NEWLINE)
-        report.write( HC_NEWLINE )    
-        report.write( 'Tickets  ' + str(self.service_requests.text)+HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( '7. Percentage of service requests that met service levels.' +HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( 'Not Applicable, There is no SLA for service requests'+HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( '8. Number of SR aged tickets 60 days (where no dependency on DVCSD)' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'Tickets  ' + str(self.tickets_more_than_sixty.text)+HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( '9. Number of issues identified from system logs' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'Tickets  ' + str(self.system_logs.text)+HC_NEWLINE)    
-        report.write( HC_NEWLINE )        
-        report.write( '10. Number of alerts/communications from DVCSD actioned' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'Tickets  ' + str(self.dvcsd_contacts.text)+HC_NEWLINE)        
-        report.write( HC_NEWLINE )    
-        report.write( '11. System up time clarify what is DVCSD and Vendor related on downtime.' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'Not Applicable' +HC_NEWLINE)                
-  
-        report.write( HC_NEWLINE )
-        report.write( 'Release Management'+HC_NEWLINE )
-        report.write( HC_NEWLINE )            
-  
-  
-        report.write( '1. Hypercare Exit on deployments major releases - Exit of hypercare after 1 month dependent no P1 or P2 raised by customer/ business in the last 7 days.' +HC_NEWLINE)
-        report.write( HC_NEWLINE )
-        report.write( 'N/A' +     HC_NEWLINE )
-        report.write( '2. Minor releases will have related hypercare of 2 weeks - no P1 or P2 raised by customer/business in the last 4 days to close that hypercare period.' +HC_NEWLINE)    
-        report.write( HC_NEWLINE )
-        report.write( 'N/A' + HC_NEWLINE )  
+    #No file interaction from the browser....
+    #with open('c:\\temp\\kpi_report_'+str(self.month.selected_value)+'_'+str(self.year.selected_value)+'.txt', 'a') as report:
+    HC_NEWLINE = '\n'
+    HC_TAB = '\t'
+    report = ''
+    report.join( 'RSA KPI Report for '+'Test Month' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'Incident Management'+HC_NEWLINE )
+    report.join( HC_NEWLINE )
+    report.join( '1. Number of Incidents in month'+HC_NEWLINE )
+
+    report.join( HC_NEWLINE )
+    report.join( 'Critical  ' + HC_TAB + str(self.critical.text) + HC_NEWLINE )
+    report.join( 'High  '  + HC_TAB+HC_TAB + str(self.high.text) + HC_NEWLINE)	
+    report.join( 'Normal  ' + HC_TAB + str(self.normal.text)+ HC_NEWLINE)
+    report.join( 'Low  '  + HC_TAB+HC_TAB + str(self.low.text)+ HC_NEWLINE)
+    report.join( 'Trivial  ' + HC_TAB + str(self.trivial.text)+ HC_NEWLINE)
+
+    report.join( HC_NEWLINE +'Failed SLAs on SIFTER response time for: ' + HC_NEWLINE)
+           
+    if len(self.sifterrows.items) == 0:
+        report.join( HC_NEWLINE +'None.' + HC_NEWLINE)
+    else:
+        report.join( HC_NEWLINE )
+        for kpifailure in self.failedresponse:
+            report.join( 'SIFTER  ' + HC_TAB + kpifailure + HC_NEWLINE )
+
+    report.join( HC_NEWLINE )
+    report.join( '1.1 SIFTER Volumes'+HC_NEWLINE )
+
+    report.join( HC_NEWLINE )
+    report.join( 'Open' + HC_TAB + HC_TAB+ str(self.open.text) + HC_NEWLINE )
+    report.join( 'Reopened'  + HC_TAB + str(self.reopened.text) + HC_NEWLINE)    
+    report.join( 'Followup' + HC_TAB + str(self.followup.text)+ HC_NEWLINE)
+    report.join( 'Resolved'  + HC_TAB + str(self.resolved.text)+ HC_NEWLINE)
+    report.join( 'Closed' + HC_TAB +HC_TAB+ str(self.closed.text)+ HC_NEWLINE)    
+    report.join( 'Total' + HC_TAB +HC_TAB+ str(self.total.text)+ HC_NEWLINE)    
+
+    report.join( HC_NEWLINE )
+    report.join( '2. Percentage of incidents that met service levels (where no dependance on DVCSD)' +HC_NEWLINE)
+    report.join( HC_NEWLINE )    
+    report.join( 'Percentage  ' + HC_TAB +  str(self.percentage_incidents_in_sla.text) + '%' +HC_NEWLINE )
+    report.join( HC_NEWLINE )
+    report.join( '3. First contact resolution rate' +HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( 'Not Applicable  All incidents raised through SIFTER'+HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( '4. Number of incident backlog/aged tickets less than 10 days old (where no dependency on DVCSD)' +HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( 'Tickets  ' + str(self.tickets_less_than_ten_days.text)+HC_NEWLINE)        
+    report.join( HC_NEWLINE )
+    report.join( '5. Unplanned Outages split into DVCSD and Vendor issues' +HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( 'Not Applicable, Production is managed by DVCSD'+HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( '6. Number of Service requests' +HC_NEWLINE)
+    report.join( HC_NEWLINE )    
+    report.join( 'Tickets  ' + str(self.service_requests.text)+HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( '7. Percentage of service requests that met service levels.' +HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( 'Not Applicable, There is no SLA for service requests'+HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( '8. Number of SR aged tickets 60 days (where no dependency on DVCSD)' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'Tickets  ' + str(self.tickets_more_than_sixty.text)+HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( '9. Number of issues identified from system logs' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'Tickets  ' + str(self.system_logs.text)+HC_NEWLINE)    
+    report.join( HC_NEWLINE )        
+    report.join( '10. Number of alerts/communications from DVCSD actioned' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'Tickets  ' + str(self.dvcsd_contacts.text)+HC_NEWLINE)        
+    report.join( HC_NEWLINE )    
+    report.join( '11. System up time clarify what is DVCSD and Vendor related on downtime.' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'Not Applicable' +HC_NEWLINE)                
+
+    report.join( HC_NEWLINE )
+    report.join( 'Release Management'+HC_NEWLINE )
+    report.join( HC_NEWLINE )            
+
+
+    report.join( '1. Hypercare Exit on deployments major releases - Exit of hypercare after 1 month dependent no P1 or P2 raised by customer/ business in the last 7 days.' +HC_NEWLINE)
+    report.join( HC_NEWLINE )
+    report.join( 'N/A' +     HC_NEWLINE )
+    report.join( '2. Minor releases will have related hypercare of 2 weeks - no P1 or P2 raised by customer/business in the last 4 days to close that hypercare period.' +HC_NEWLINE)    
+    report.join( HC_NEWLINE )
+    report.join( 'N/A' + HC_NEWLINE )  
+
+    print (report)
 
 
 
